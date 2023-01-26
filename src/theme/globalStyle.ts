5f307623c4df7e683ebcd6457c77c2a29fd4a2f3
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-01-26 10:35:29 -0300

    created: global typography style

---
+++ src/theme/globalStyle.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-size: 1.6rem;
+    font-family: 'Poppins', sans-serif;
+  }
+`
