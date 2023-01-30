81cc3cadcb41b7a7a50a1451eeec0bf97a6a6c56
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-01-30 11:59:22 -0300

    test: adding participants if input isn't empty

--- src/components/addParticipantsForm/addParticipantsForm.test.tsx
+++ src/components/addParticipantsForm/addParticipantsForm.test.tsx
@@ -1,21 +1,46 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { AddParticipantsForm } from ".";
+
+function getHTMLElementsFromFormComponent() {
+  const getInputByPlaceholder = screen.getByPlaceholderText('Insira o nome dos participantes');
+  const getButtonByRole = screen.getByRole('button');
+
+  return {
+    input: getInputByPlaceholder,
+    button: getButtonByRole
+  }
+}
 
 test('should not be able to add participants when input is empty', () => {
   render(<AddParticipantsForm />);
-
-  function getHTMLElementsFromFormComponent() {
-    const getInputByPlaceholder = screen.getByPlaceholderText('Insira o nome dos participantes');
-    const getButtonByRole = screen.getByRole('button');
-
-    return {
-      input: getInputByPlaceholder,
-      button: getButtonByRole
-    }
-  }
 
   const { input, button } = getHTMLElementsFromFormComponent();
 
   expect(input).toBeInTheDocument();
   expect(button).toBeDisabled();
+})
+
+test('it should add a participant if it has a filled name on input', () => {
+  render(<AddParticipantsForm />)
+
+  const { input, button } = getHTMLElementsFromFormComponent();
+
+  function setParticipantNameOnInput() {
+    fireEvent.change(input, { target: { value: 'Thiago' } });
+  }
+
+  function submitParticipantName() {
+    fireEvent.click(button)
+  }
+
+  setParticipantNameOnInput();
+  submitParticipantName();
+
+  expect(input).toHaveFocus();
+  expect(input).toHaveValue("");
+  /*
+    1. pegar o input e o button na DOM [x]
+    2. preencher o input com algum nome [x]
+    3. utilizar o hook de adicionar participante ao clicar no button
+  */
 })
