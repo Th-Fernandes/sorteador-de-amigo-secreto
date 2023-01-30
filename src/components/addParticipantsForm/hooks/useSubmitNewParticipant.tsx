9784e2ab1336accbe1ff08bd3d210cec34c57c6f
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-01-30 12:41:18 -0300

    created: useSubmitNewParticipant hook

---
+++ src/components/addParticipantsForm/hooks/useSubmitNewParticipant.tsx
@@ -0,0 +1,22 @@
+import { useState, useRef } from "react";
+import { useAddParticipant } from "../../../state/hooks/useAddParticipant";
+
+export function useSubmitNewParticipant() {
+  const [participantName, setParticipantName] = useState<string>("");
+  const addParticipantOnParticipantsList = useAddParticipant();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  function submitParticipant(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    addParticipantOnParticipantsList(participantName);
+    setParticipantName("");
+    inputRef.current?.focus();
+  }
+
+  return { 
+    participantName, 
+    setParticipantName, 
+    inputRef, 
+    submitParticipant 
+  }
+}
