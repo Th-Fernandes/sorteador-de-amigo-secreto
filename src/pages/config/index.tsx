7012d400a89d542bc7ec56c46608d794d7c30193
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-02-14 10:06:56 -0300

    refactor: using app model on config page

--- src/pages/config/index.tsx
+++ src/pages/config/index.tsx
@@ -1,18 +1,16 @@
 import { AddParticipantsForm } from "./components/addParticipantsForm";
-import { Card } from "../../components/Card";
-import { Header } from "components/Header";
 import { ParticipantsList } from "./components/ParticipantsList";
 import { Footer } from "./components/Footer";
+import App from "App";
 
 export function ConfigPage() {
   return (
-    <>
-      <Header />
-      <Card>
+    <App>
+      <main>
         <AddParticipantsForm />
         <ParticipantsList />
-        <Footer />
-      </Card>
-    </>
+      </main>
+      <Footer />
+    </App>
   );
 }
