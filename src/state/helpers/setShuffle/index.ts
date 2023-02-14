8b27744d7773fdbb5ebb54a9b693d591581e8943
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-02-14 09:33:41 -0300

    refactor: setShuffle helper

--- src/state/helpers/setShuffle/index.ts
+++ src/state/helpers/setShuffle/index.ts
@@ -3,17 +3,14 @@ import shuffle from "just-shuffle";
 
 export function setShuffle(participantsList: ParticipantsList) {
   const participantsQuantity = participantsList.length;
-  const shuffledParticipantsList = shuffle(participantsList);
+  const shuffledList = shuffle(participantsList);
   const shuffleResult = new Map<string, string>();
 
   for (let index = 0; index < participantsQuantity; index++) {
     const lastParticipantIndex = participantsQuantity - 1;
     const shuffleIndex = index === lastParticipantIndex ? 0 : index + 1;
 
-    shuffleResult.set(
-      shuffledParticipantsList[index],
-      shuffledParticipantsList[shuffleIndex]
-    );
+    shuffleResult.set(shuffledList[index], shuffledList[shuffleIndex]);
   } 
 
   return shuffleResult;
