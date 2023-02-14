d58a03c131450a383883985136a6a2f89fb36109
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-02-14 10:04:51 -0300

    refactor: app global model

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { AddParticipantsForm } from "./pages/config/components/addParticipantsForm"
 import { Card } from "./components/Card";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./theme/globalStyle";
 import { ThemeComponent } from "./theme/theme";
-import { Footer } from "pages/config/components/Footer";
+import { ReactNode } from "react";
 
-export default function App() {
+interface AppProps {
+  children: ReactNode
+}
+
+export default function App({children}:AppProps) {
   return (
     <>
       <GlobalStyle />
       <ThemeComponent>
         <Header />
         <Card>
-          <main>
-            <AddParticipantsForm />
-          </main>
-          <Footer />
+          {children}
         </Card>
       </ThemeComponent>
     </>
