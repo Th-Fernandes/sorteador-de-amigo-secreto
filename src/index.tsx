0680d6bf4028f244d7ceaa39e8a68ca656c6cbd6
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-02-14 10:06:01 -0300

    refactor: added results page path

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import 'material-icons/iconfont/material-icons.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+import { ConfigPage } from 'pages/config';
+import { ResultsPage } from 'pages/results';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <RecoilRoot>
         <Routes>
-          <Route path='/' element={<App/>} />
+          <Route path='/' element={<ConfigPage/>} />
+          <Route path='/results' element={<ResultsPage/>} />
         </Routes>
       </RecoilRoot>
     </BrowserRouter>
