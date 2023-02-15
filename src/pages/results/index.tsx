d17686ccfcc613d9ea1da246abcd3285bc4951ea
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-02-15 09:49:05 -0300

    refactor: removing participant selector test

--- src/pages/results/index.tsx
+++ src/pages/results/index.tsx
@@ -1,54 +1,15 @@
 import App from "App";
 import { Heading } from "components/Heading";
-import { FormEvent, useState } from "react";
-import { useParticipantsList } from "state/hooks/useParticipantsList";
-import { useShuffleResult } from "state/hooks/useShuffleResult";
 import { Footer } from "./components/footer";
+import { ShuffledParticipantSelector } from "./components/ShuffledParticipantSelector";
 
 export function ResultsPage() {
-  const participantsList = useParticipantsList();
-  const [selectedParticipant, setSelectedParticipant] = useState<string>("");
-  const [secretFriend, setSecretFriend] = useState<string>("");
-  const shuffleResult = useShuffleResult();
-
-  function handleSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-
-    if (shuffleResult.has(selectedParticipant)) {
-      setSecretFriend(shuffleResult.get(selectedParticipant)!);
-    }
-  }
-
   return (
     <App>
       <main>
         <Heading>Quem vai tirar o papelzinho?</Heading>
-
-        <form onSubmit={handleSubmit}>
-          <select
-            required
-            name="selectedParticipant"
-            id="selectedParticipant"
-            placeholder="Selecione seu nome"
-            value={selectedParticipant}
-            onChange={(event) => setSelectedParticipant(event.target.value)}
-          >
-            {participantsList.map((participant) => (
-              <option key={participant}>{participant}</option>
-            ))}
-          </select>
-
-          <span>Clique em em sortear para ver quem é seu amigo secreto!</span>
-          
-          <div>
-            <button>
-              <img src="/img/casino.png" alt="" />
-              Sortear!
-            </button>
-            {secretFriend && <p role="alert">{secretFriend}</p>}
-          </div>
-        </form>
-      </main>
+        <ShuffledParticipantSelector />
+      </main>  
       <Footer />
     </App>
   );
--- src/pages/results/results.test.tsx
+++ src/pages/results/results.test.tsx
@@ -1,7 +1,6 @@
-import { fireEvent, render, screen } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { RecoilRoot } from "recoil";
 import { useParticipantsList } from "state/hooks/useParticipantsList";
-import { useShuffleResult } from "state/hooks/useShuffleResult";
 import { ResultsPage } from ".";
 
 jest.mock('state/hooks/useParticipantsList.ts', () => {
@@ -9,53 +8,22 @@ jest.mock('state/hooks/useParticipantsList.ts', () => {
     useParticipantsList: jest.fn()
   }
 })
-jest.mock('state/hooks/useShuffleResult', () => {
-  return {
-    useShuffleResult: jest.fn()
-  }
-})
 
 describe('results page', () => {
   const participants = ['Joe', 'Ellen', 'Lea'];
-  const shuffleResult = new Map([
-    ['Joe', 'Ellen'], ['Ellen', 'Lea'], ['Lea', 'Joe']
-  ])
 
   beforeEach(() => {
     (useParticipantsList as jest.Mock).mockReturnValue(participants);
-    (useShuffleResult as jest.Mock).mockReturnValue(shuffleResult);
   })
 
   test('should allow all participants to show your secret friend', () => {
-    render(<ResultsPage/>);  
-
-    const optionsByRole = screen.queryAllByRole('option');
-    expect(optionsByRole).toHaveLength(participants.length);
-  })
-
-  test('should show secret friend when requested', () => {
     render(
       <RecoilRoot>
         <ResultsPage/>
       </RecoilRoot>
-    );
+    );  
 
-   
-    const select = screen.getByPlaceholderText('Selecione seu nome')
-        
-    fireEvent.change(select, {
-        target: {
-            value: participants[0]
-        }
-    })
-
-    const botao = screen.getByRole('button')
-
-    fireEvent.click(botao)
-
-    const amigoSecreto = screen.getByRole('alert')
-
-    expect(amigoSecreto).toBeInTheDocument()
-
+    const optionsByRole = screen.queryAllByRole('option');
+    expect(optionsByRole).toHaveLength(participants.length);
   })
 })
