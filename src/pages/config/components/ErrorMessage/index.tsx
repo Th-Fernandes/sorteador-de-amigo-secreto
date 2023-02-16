8718ed791637c122c40444168f97dde99d395215
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-02-16 10:31:56 -0300

    refactor: errorMessage styles

--- src/pages/config/components/ErrorMessage/index.tsx
+++ src/pages/config/components/ErrorMessage/index.tsx
@@ -1,13 +1,14 @@
-import { useErrorMessage } from "state/hooks/useErrorMessage"
+import { useErrorMessage } from "state/hooks/useErrorMessage";
+import { StyledErrorMessage } from "./styles";
 
 export function ErrorMessage() {
   const errorMessage = useErrorMessage();
-  
+
   return (
     <>
-      {
-        errorMessage && <p role="alert">{errorMessage}</p>
-      }
+      {errorMessage && (
+        <StyledErrorMessage role="alert">{errorMessage}</StyledErrorMessage>
+      )}
     </>
-  )
+  );
 }
---
+++ src/pages/config/components/ErrorMessage/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledErrorMessage = styled.p`
+  text-align: center;
+  color: #ff0011;
+  margin-top: 1.2rem;
+`
